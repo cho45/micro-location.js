5f369675fdf130317857fea48343f24e43954ef2
cho45 <cho45@lowreal.net>
2011-05-24 17:14:58 +0900

    update test

--- test.js
+++ test.js
@@ -1,40 +1,69 @@
-#!node
+#!/usr/bin/env node
+TAP();
 
 var Location = require('./location.js').Location;
-var util = require('util');
 
 var l;
 
 l = Location.parse('http://example.com/');
-console.log(l.href == 'http://example.com/');
+is(l.href, 'http://example.com/');
 
 l = Location.parse('http://example.com/#baz');
-console.log(l.href == 'http://example.com/#baz');
+is(l.href, 'http://example.com/#baz');
 
 l = Location.parse('http://example.com/?foo=bar');
-console.log(l.href == 'http://example.com/?foo=bar');
+is(l.href, 'http://example.com/?foo=bar');
 
 l = Location.parse('http://example.com/?foo=bar#baz');
-console.log(l.href == 'http://example.com/?foo=bar#baz');
-console.log(l.params('foo') == 'bar');
+is(l.href, 'http://example.com/?foo=bar#baz');
+is(l.params('foo'), 'bar');
 
 l = l.params({
 	foo : 'bar',
 	baz : ['aaa', 'bbb']
 });
-console.log(l.href == 'http://example.com/?foo=bar&baz=aaa&baz=bbb#baz');
+is(l.href, 'http://example.com/?foo=bar&baz=aaa&baz=bbb#baz');
 
 l = Location.parse('//example.com/foo');
-console.log(l.href == '//example.com/foo');
+is(l.href, '//example.com/foo');
 
 l = Location.parse('/');
-console.log(l.href == '/');
-console.log(l.pathname == '/');
+is(l.href, '/');
+is(l.pathname, '/');
 
 l = l.params({ foo : 'bar' });
-console.log(l.href == '/?foo=bar');
+is(l.href, '/?foo=bar');
 
 l = Location.parse('http://example.com/');
 l.params({ foo : 'bar' });
-console.log(l.href == 'http://example.com/');
-console.log(l.params('foo') == null);
+is(l.href, 'http://example.com/');
+is(l.params('foo'), null);
+
+done_testing();
+
+
+function TAP () {
+	var util = require('util');
+	this.ok = function (bool, name) {
+		done_testing.n++;
+		var r = bool ? 'ok' : 'not ok';
+		r += " " + done_testing.n;
+		console.log(name ? r + " # " + name : r);
+	};
+	this.is = function (got, expected, name) {
+		got = util.inspect(got, true, 2);
+		expected = util.inspect(expected, true, 2);
+		if (got === expected) {
+			ok(true, name);
+		} else {
+			ok(false, name);
+			console.log("# got:\n" + got.replace(/^/m, "# "));
+			console.log("# expected:\n" + expected.replace(/^/m, "# "));
+		}
+	}
+	this.done_testing = function () {
+		var n = done_testing.n;
+		console.log('1..' + n);
+	}
+	this.done_testing.n = 0;
+}
